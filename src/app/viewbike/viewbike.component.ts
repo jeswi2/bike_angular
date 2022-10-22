32592e94e92331a4051114169b12eca295f7e126
jeswi2 <jeswimathewm@gmail.com>
2022-10-22 23:33:57 +0530

    vi

---
+++ src/app/viewbike/viewbike.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewbike',
+  templateUrl: './viewbike.component.html',
+  styleUrls: ['./viewbike.component.css']
+})
+export class ViewbikeComponent implements OnInit {
+
+  constructor() { }
+
+
+  bikedata:any=[]
+
+  ngOnInit(): void {
+  }
+
+}
